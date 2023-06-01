ce9ebc32e97609a893317f647935c8fab992d2a7
abdul hannan <shohas563@gmail.com>
2023-06-01 18:40:02 -0400

    .env setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log('database is connected successfully');
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+
+    } catch (err) {
+        console.log('failed to load data', err);
+
+    }
+}
+bootstrap();
